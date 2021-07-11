7075b75529af6aae0a5ec0fcc77b7a5d5b84d6bc
Ojutalayo Aseoluwa <83563462+AshlyX3@users.noreply.github.com>
2021-07-11 03:15:56 -0700

    Create navbar.php
    The php code  for the navigation area of the website

---
+++ navbar.php
@@ -0,0 +1,20 @@
+
+<!-- NAVIGATION BAR -->
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="#">myPOST app</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="index.php">login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="register.php">register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
