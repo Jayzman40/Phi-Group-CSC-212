622bc2eea05a0f3aca38b89df849c72e59a7e8f6
Abdussomad20 <59209211+Abdussomad20@users.noreply.github.com>
2021-07-13 12:54:57 +0100

    head.php

---
+++ head.php
@@ -0,0 +1,13 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dependencies/bootstrap5/css/bootstrap.css">
+    <link rel="stylesheet" href="dependencies/css/style.css">
+    <title> Phi Group - Computing </title>
+</head>
+<body>
