cfefe62b3fc522b16286fcdd1034ecd342ce752c
alhassanmuhd <85667756+alhassanmuhd@users.noreply.github.com>
2021-07-20 18:06:52 +0100

    Create navbar.php

---
+++ posts/navbar.php
@@ -0,0 +1,25 @@
+<!-- NAVIGATION BAR -->
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="posts.php">PHI GROUP - FACULTY OF COMPUTING</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link text-white" aria-current="page" href="newpost.php?">Create New Post</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" aria-current="page" href="myposts.php">My Posts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" aria-current="page" href="posts.php">All Posts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="../logout.php">Log out</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
